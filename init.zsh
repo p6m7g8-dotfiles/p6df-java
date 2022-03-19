9221c7b8e19bca736afe363a58d273cd2ac21ecd
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-18 20:49:13 -0400

    chore: relocates to p6m7g8-dotfiles

--- init.zsh
+++ init.zsh
@@ -7,7 +7,7 @@
 ######################################################################
 p6df::modules::java::deps() {
   ModuleDeps=(
-    p6m7g8/p6common
+    p6m7g8-dotfiles/p6common
     gcuisinier/jenv
   )
 }
